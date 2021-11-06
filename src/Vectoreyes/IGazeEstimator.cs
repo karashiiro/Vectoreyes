2407be44620a1aee2ba40ae92103252a6387329e
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-11-06 11:50:13 -0700

    Improve doc comments

--- src/Vectoreyes/IGazeEstimator.cs
+++ src/Vectoreyes/IGazeEstimator.cs
@@ -5,7 +5,7 @@
         /// <summary>
         /// Estimates a gaze vector from the provided image. The image
         /// provided should be a single-channel greyscale image containing
-        /// both the eye center and the corner of the eye.
+        /// both the eye center and the inner corner of the eye.
         /// </summary>
         /// <param name="image">The greyscale eye image.</param>
         /// <returns>The predicted gaze and associated eye center.</returns>
@@ -14,7 +14,7 @@
         /// <summary>
         /// Estimates a gaze vector from the provided image. The image
         /// provided should be a single-channel greyscale image containing
-        /// both the eye center and the corner of the eye.
+        /// both the eye center and the inner corner of the eye.
         /// </summary>
         /// <param name="image">The greyscale eye image.</param>
         /// <param name="r">The number of rows in the image.</param>
