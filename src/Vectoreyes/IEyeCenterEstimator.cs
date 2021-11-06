28d0da65f994870230e635abd5321b00995a5eb0
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-11-06 11:49:52 -0700

    Improve doc comments

--- src/Vectoreyes/IEyeCenterEstimator.cs
+++ src/Vectoreyes/IEyeCenterEstimator.cs
@@ -6,7 +6,7 @@
         /// Estimates an eye center location in the provided image. The image
         /// provided should be a single-channel greyscale image.
         /// </summary>
-        /// <param name="image">The greyscale image.</param>
+        /// <param name="image">The greyscale eye image.</param>
         /// <returns>The predicted eye center.</returns>
         EyeCenter EstimateCenter(byte[][] image);
 
@@ -14,7 +14,7 @@
         /// Estimates an eye center location in the provided image. The image
         /// provided should be a single-channel greyscale image.
         /// </summary>
-        /// <param name="image">The greyscale image.</param>
+        /// <param name="image">The greyscale eye image.</param>
         /// <param name="r">The number of rows in the image.</param>
         /// <param name="c">The number of columns in the image.</param>
         /// <returns>The predicted eye center.</returns>
--- src/Vectoreyes/IGazeEstimator.cs
+++ src/Vectoreyes/IGazeEstimator.cs
@@ -4,17 +4,19 @@
     {
         /// <summary>
         /// Estimates a gaze vector from the provided image. The image
-        /// provided should be a single-channel greyscale image.
+        /// provided should be a single-channel greyscale image containing
+        /// both the eye center and the corner of the eye.
         /// </summary>
-        /// <param name="image">The greyscale image.</param>
+        /// <param name="image">The greyscale eye image.</param>
         /// <returns>The predicted gaze and associated eye center.</returns>
         Gaze EstimateGaze(byte[][] image);
 
         /// <summary>
         /// Estimates a gaze vector from the provided image. The image
-        /// provided should be a single-channel greyscale image.
+        /// provided should be a single-channel greyscale image containing
+        /// both the eye center and the corner of the eye.
         /// </summary>
-        /// <param name="image">The greyscale image.</param>
+        /// <param name="image">The greyscale eye image.</param>
         /// <param name="r">The number of rows in the image.</param>
         /// <param name="c">The number of columns in the image.</param>
         /// <returns>The predicted gaze and associated eye center.</returns>
