0c8804077a77f751c5832a7385fe87bc7d3258dc
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-11-08 10:47:35 -0800

    Remove poor interfaces

--- src/Vectoreyes/VectoreyesEstimator.cs
+++ src/Vectoreyes/VectoreyesEstimator.cs
@@ -5,32 +5,71 @@ using Vectoreyes.Gazes;
 
 namespace Vectoreyes
 {
-    public class VectoreyesEstimator : IEyeCenterEstimator, IGazeEstimator
+    public class VectoreyesEstimator
     {
-        public EyeCenter EstimateCenter(Bitmap image)
+        /// <summary>
+        /// Estimates an eye center location in the provided image.
+        /// </summary>
+        /// <param name="image">The eye image.</param>
+        /// <returns>The predicted eye center.</returns>
+        public static EyeCenter EstimateCenter(Bitmap image)
         {
             var greyImage = Utils.Bitmap2GreyArray(image);
             return EstimateCenter(greyImage, image.Height, image.Width);
         }
 
-        public EyeCenter EstimateCenter(float[] image, int rows, int cols)
+        /// <summary>
+        /// Estimates an eye center location in the provided image. The image
+        /// provided should be a non-normalized single-channel greyscale image
+        /// in float format.
+        /// </summary>
+        /// <param name="image">The greyscale eye image.</param>
+        /// <param name="rows">The number of rows in the image.</param>
+        /// <param name="cols">The number of columns in the image.</param>
+        /// <returns>The predicted eye center.</returns>
+        public static EyeCenter EstimateCenter(float[] image, int rows, int cols)
         {
             var imageCopy = Utils.CopyFloatImage(image);
             return new EyeCenterEstimator(rows, cols).Estimate(imageCopy);
         }
 
-        public EyeCenterEstimator CreateReusableEstimator(int rows, int cols)
+        /// <summary>
+        /// Creates an eye center estimator with prebuilt buffers for a specific
+        /// image size. The estimator may not be used for images of different sizes
+        /// than it was created for.
+        /// </summary>
+        /// <param name="rows">The number of rows in the image.</param>
+        /// <param name="cols">The number of columns in the image.</param>
+        /// <returns>The reusable eye center estimator.</returns>
+        public static EyeCenterEstimator CreateReusableEstimator(int rows, int cols)
         {
             return new EyeCenterEstimator(rows, cols);
         }
 
-        public Gaze EstimateGaze(Bitmap image)
+        /// <summary>
+        /// Estimates a gaze vector from the provided image. The image
+        /// provided should contain both the eye center and the
+        /// inner corner of the eye.
+        /// </summary>
+        /// <param name="image">The eye image.</param>
+        /// <returns>The predicted gaze and associated eye center.</returns>
+        public static Gaze EstimateGaze(Bitmap image)
         {
             var greyImage = Utils.Bitmap2GreyArray(image);
             return EstimateGaze(greyImage, image.Height, image.Width);
         }
 
-        public Gaze EstimateGaze(float[] image, int rows, int cols)
+        /// <summary>
+        /// Estimates a gaze vector from the provided image. The image
+        /// provided should be a non-normalized single-channel greyscale
+        /// image in float format containing both the eye center and the
+        /// inner corner of the eye.
+        /// </summary>
+        /// <param name="image">The greyscale eye image.</param>
+        /// <param name="rows">The number of rows in the image.</param>
+        /// <param name="cols">The number of columns in the image.</param>
+        /// <returns>The predicted gaze and associated eye center.</returns>
+        public static Gaze EstimateGaze(float[] image, int rows, int cols)
         {
             throw new NotImplementedException();
         }
