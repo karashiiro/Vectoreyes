ddbbed95aa07490c4af3170d5b44e33fcb5ec54e
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-11-09 13:29:13 -0800

    Add exposed function for pointer image on eye center predictor

--- src/Vectoreyes/EyeCenters/EyeCenterEstimator.cs
+++ src/Vectoreyes/EyeCenters/EyeCenterEstimator.cs
@@ -5,7 +5,7 @@ namespace Vectoreyes.EyeCenters
     /// <summary>
     /// Eye center estimation class with pre-built buffers for reuse. This class is not thread-safe.
     /// </summary>
-    public unsafe class EyeCenterEstimator
+    public class EyeCenterEstimator
     {
         private readonly float[] _imageBlurred;
         private readonly float[] _gradResultX;
@@ -42,121 +42,134 @@ namespace Vectoreyes.EyeCenters
         /// </summary>
         /// <param name="image">The image to search.</param>
         /// <returns>An eye center estimate.</returns>
-        public EyeCenter Estimate(float[] image)
+        public unsafe EyeCenter Estimate(float[] image)
         {
             fixed (float* im = &image[0])
             {
-                // We can't meaningfully calculate anything on an image this small.
-                if (_rows < 4 && _cols < 4)
-                {
-                    return new EyeCenter(-1, -1);
-                }
+                return Estimate(im);
+            }
+        }
 
-                // Zero scores
-                for (var i = 0; i < _length; i++)
-                {
-                    _centerScores[i] = 0;
-                }
+        /// <summary>
+        /// Estimates an eye center location from an image. The array passed in will be modified.
+        /// 
+        /// Implemented based on Timm, F. and Barth, E. (2011). "Accurate eye centre localisation by means of gradients",
+        /// with modifications from https://thume.ca/projects/2012/11/04/simple-accurate-eye-center-tracking-in-opencv.
+        /// </summary>
+        /// <param name="image">The image to search.</param>
+        /// <returns>An eye center estimate.</returns>
+        public unsafe EyeCenter Estimate(float* image)
+        {
+            // We can't meaningfully calculate anything on an image this small.
+            if (_rows < 4 && _cols < 4)
+            {
+                return new EyeCenter(-1, -1);
+            }
 
-                // Blur step:
-                fixed (float* imageBlurred = &_imageBlurred[0])
-                {
-                    // Radius chosen experimentally
-                    GaussianBlur.Blur(im, imageBlurred, _rows, _cols, (int)Math.Sqrt(Math.Min(_rows, _cols)) / 2);
+            // Zero scores
+            for (var i = 0; i < _length; i++)
+            {
+                _centerScores[i] = 0;
+            }
 
-                    // Calculate gradients, gradient magnitude mean, and gradient magnitude std
-                    fixed (float* gradResultX = &_gradResultX[0])
+            // Blur step:
+            fixed (float* imageBlurred = &_imageBlurred[0])
+            {
+                // Radius chosen experimentally
+                GaussianBlur.Blur(image, imageBlurred, _rows, _cols, (int)Math.Sqrt(Math.Min(_rows, _cols)) / 2);
+
+                // Calculate gradients, gradient magnitude mean, and gradient magnitude std
+                fixed (float* gradResultX = &_gradResultX[0])
+                {
+                    fixed (float* gradResultY = &_gradResultY[0])
                     {
-                        fixed (float* gradResultY = &_gradResultY[0])
+                        CV.CentralDifferenceGradientX(imageBlurred, gradResultX, _rows, _cols);
+                        CV.CentralDifferenceGradientY(imageBlurred, gradResultY, _rows, _cols);
+
+                        fixed (float* gradResult = &_gradResult[0])
                         {
-                            CV.CentralDifferenceGradientX(imageBlurred, gradResultX, _rows, _cols);
-                            CV.CentralDifferenceGradientY(imageBlurred, gradResultY, _rows, _cols);
-
-                            fixed (float* gradResult = &_gradResult[0])
+                            fixed (float* gradMags = &_gradMags[0])
                             {
-                                fixed (float* gradMags = &_gradMags[0])
+                                for (var r = 0; r < _rows; r++)
                                 {
-                                    for (var r = 0; r < _rows; r++)
+                                    for (var c = 0; c < _cols; c++)
                                     {
-                                        for (var c = 0; c < _cols; c++)
-                                        {
-                                            gradMags[r * _cols + c] = (float)Math.Sqrt(gradResultX[r * _cols + c] * gradResultX[r * _cols + c] + gradResultY[r * _cols + c] * gradResultY[r * _cols + c]);
-                                        }
+                                        gradMags[r * _cols + c] = (float)Math.Sqrt(gradResultX[r * _cols + c] * gradResultX[r * _cols + c] + gradResultY[r * _cols + c] * gradResultY[r * _cols + c]);
                                     }
+                                }
 
-                                    var gradMagMean = Utils.Mean2D(gradMags, _rows, _cols);
-                                    var gradMagStd = Utils.Std2D(gradMags, _rows, _cols, gradMagMean);
-                                    var gradThreshold = 0.9f * gradMagStd + gradMagMean;
+                                var gradMagMean = Utils.Mean2D(gradMags, _rows, _cols);
+                                var gradMagStd = Utils.Std2D(gradMags, _rows, _cols, gradMagMean);
+                                var gradThreshold = 0.9f * gradMagStd + gradMagMean;
 
-                                    for (var r = 0; r < _rows; r++)
+                                for (var r = 0; r < _rows; r++)
+                                {
+                                    for (var c = 0; c < _cols; c++)
                                     {
-                                        for (var c = 0; c < _cols; c++)
+                                        var gradMag = gradMags[r * _cols + c];
+
+                                        // Ignore all gradients below a threshold
+                                        if (gradMag < gradThreshold)
                                         {
-                                            var gradMag = gradMags[r * _cols + c];
+                                            continue;
+                                        }
 
-                                            // Ignore all gradients below a threshold
-                                            if (gradMag < gradThreshold)
+                                        // Scale gradients to unit length
+                                        gradResult[r * _cols + c + 1] = gradResultY[r * _cols + c] / gradMag;
+                                        gradResult[r * _cols + c] = gradResultX[r * _cols + c] / gradMag;
+                                    }
+                                }
+                            }
+
+                            // Predict eye center:
+                            // To save time, we only calculate the objective for every Kth column/row,
+                            // where K is the square root of the product of the rows and columns in the
+                            // image.
+                            //
+                            // This gives us a rough approximation that we can then refine by repeatedly
+                            // square rooting the step size and calculating scores within a predicted region.
+                            // This saves us a huge number of Score() calculations and allows us to
+                            // calculate eye centers in high-resolution images in realistic amounts of time.
+                            var initialStep = (int)Math.Sqrt(_rows * _cols);
+
+                            fixed (float* centerScores = &_centerScores[0])
+                            {
+                                // Initial step scoring
+                                for (var r = 0; r < _rows; r += initialStep)
+                                {
+                                    for (var c = 0; c < _cols; c += initialStep)
+                                    {
+                                        centerScores[r * _cols + c] = Score(gradResult, _rows, _cols, r, c);
+                                    }
+                                }
+
+                                // Search for better and better objectives within regions with high surrounding
+                                // objectives. We stop at a step of 2, sacrificing negligible accuracy for a
+                                // significant speedup on larger images.
+                                for (var lastStep = initialStep; lastStep > 2; lastStep = (int)Math.Sqrt(lastStep))
+                                {
+                                    var (localMaxR, localMaxC) = Utils.Argmax2D(centerScores, _rows, _cols);
+                                    var localMaxVal = centerScores[localMaxR * _cols + localMaxC];
+                                    var approxThreshold = localMaxVal * 0.999999f;
+                                    var step = (int)Math.Sqrt(lastStep);
+                                    for (var r = 0; r < _rows; r += step)
+                                    {
+                                        for (var c = 0; c < _cols; c += step)
+                                        {
+                                            var scoreR = Math.Min(_rows - 1, (int)(Math.Round(r / (float)lastStep) * lastStep));
+                                            var scoreC = Math.Min(_cols - 1, (int)(Math.Round(c / (float)lastStep) * lastStep));
+                                            if (centerScores[scoreR * _cols + scoreC] > approxThreshold)
                                             {
-                                                continue;
+                                                centerScores[r * _cols + c] = Score(gradResult, _rows, _cols, r, c);
                                             }
-
-                                            // Scale gradients to unit length
-                                            gradResult[r * _cols + c + 1] = gradResultY[r * _cols + c] / gradMag;
-                                            gradResult[r * _cols + c] = gradResultX[r * _cols + c] / gradMag;
                                         }
                                     }
                                 }
 
-                                // Predict eye center:
-                                // To save time, we only calculate the objective for every Kth column/row,
-                                // where K is the square root of the product of the rows and columns in the
-                                // image.
-                                //
-                                // This gives us a rough approximation that we can then refine by repeatedly
-                                // square rooting the step size and calculating scores within a predicted region.
-                                // This saves us a huge number of Score() calculations and allows us to
-                                // calculate eye centers in high-resolution images in realistic amounts of time.
-                                var initialStep = (int)Math.Sqrt(_rows * _cols);
+                                // Calculate final estimated center
+                                var (maxR, maxC) = Utils.Argmax2D(centerScores, _rows, _cols);
 
-                                fixed (float* centerScores = &_centerScores[0])
-                                {
-                                    // Initial step scoring
-                                    for (var r = 0; r < _rows; r += initialStep)
-                                    {
-                                        for (var c = 0; c < _cols; c += initialStep)
-                                        {
-                                            centerScores[r * _cols + c] = Score(gradResult, _rows, _cols, r, c);
-                                        }
-                                    }
-
-                                    // Search for better and better objectives within regions with high surrounding
-                                    // objectives. We stop at a step of 2, sacrificing negligible accuracy for a
-                                    // significant speedup on larger images.
-                                    for (var lastStep = initialStep; lastStep > 2; lastStep = (int)Math.Sqrt(lastStep))
-                                    {
-                                        var (localMaxR, localMaxC) = Utils.Argmax2D(centerScores, _rows, _cols);
-                                        var localMaxVal = centerScores[localMaxR * _cols + localMaxC];
-                                        var approxThreshold = localMaxVal * 0.999999f;
-                                        var step = (int)Math.Sqrt(lastStep);
-                                        for (var r = 0; r < _rows; r += step)
-                                        {
-                                            for (var c = 0; c < _cols; c += step)
-                                            {
-                                                var scoreR = Math.Min(_rows - 1, (int)(Math.Round(r / (float)lastStep) * lastStep));
-                                                var scoreC = Math.Min(_cols - 1, (int)(Math.Round(c / (float)lastStep) * lastStep));
-                                                if (centerScores[scoreR * _cols + scoreC] > approxThreshold)
-                                                {
-                                                    centerScores[r * _cols + c] = Score(gradResult, _rows, _cols, r, c);
-                                                }
-                                            }
-                                        }
-                                    }
-
-                                    // Calculate final estimated center
-                                    var (maxR, maxC) = Utils.Argmax2D(centerScores, _rows, _cols);
-
-                                    return new EyeCenter(maxC, maxR);
-                                }
+                                return new EyeCenter(maxC, maxR);
                             }
                         }
                     }
@@ -172,7 +185,7 @@ namespace Vectoreyes.EyeCenters
         /// Implemented based on Timm, F. and Barth, E. (2011). "Accurate eye centre localisation by means of gradients",
         /// with modifications from https://thume.ca/projects/2012/11/04/simple-accurate-eye-center-tracking-in-opencv.
         /// </summary>
-        private static float Score(float* gradient, int rows, int cols, int centerR, int centerC)
+        private static unsafe float Score(float* gradient, int rows, int cols, int centerR, int centerC)
         {
             var score = 0f;
             for (var r = 0; r < rows; r++)
